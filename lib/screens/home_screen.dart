b38f7c2a7f3008fbd5ba8fc0382b411b978894da
limit-b <jehan082@gmail.com>
2023-04-17 01:35:11 +0900

    추가: 웹툰 데이터 API
    웹툰 Data fetch (기본 방식)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,142 +1,48 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
+  bool isLoading = true;
+  List<WebtoonModel> webtoons = [];
 
-  bool isRunning = false;
-  int totalSeconds = twentyFiveMinutes;
-  int totalPomodoros = 0;
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
 
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros += 1;
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-      });
-
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds -= 1;
-      });
-    }
+    setState(() {});
   }
 
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausedPressed() {
-    timer.cancel();
-
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-
-    return duration.toString().split(".").first.substring(2, 7);
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
   }
 
   @override
   Widget build(BuildContext context) {
+    print("==> webtoons: $webtoons");
+    print("==> isLoading: $isLoading");
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 90.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+      appBar: AppBar(
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24.0,
           ),
-          Flexible(
-            flex: 3,
-            child: Center(
-              child: IconButton(
-                iconSize: 120.0,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausedPressed : onStartPressed,
-                icon: Icon(
-                  isRunning
-                      ? Icons.pause_circle_outlined
-                      : Icons.play_circle_outlined,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(50.0),
-                        topRight: Radius.circular(50.0),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Pomodoros",
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "$totalPomodoros",
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                            fontSize: 58.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
+        elevation: 2.0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
       ),
+      backgroundColor: Colors.white,
     );
   }
 }
